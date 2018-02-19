6eb52d8a28a1b19b3e05fdc81c340d709e821159
foreachsam <foreachsam@users.noreply.github.com>
2018-02-19 11:14:37 +0800

    install script

---
+++ example/howto/install/install-from-apt/demo-002/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+## https://gstreamer.freedesktop.org/documentation/installing/on-linux.html#install-gstreamer-on-ubuntu-or-debian
+## https://foreachsam.github.io/book-framework-gstreamer/read/howto/install/install-from-apt/
+
+sudo apt-get install $(cat package.txt)
